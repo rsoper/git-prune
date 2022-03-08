b1fdc0475696bc9145ee32322221707b8fa5d12e
Richard <20671094+rsoper@users.noreply.github.com>
2022-03-08 14:11:39 -0800

    Remove "master" branch failure (#7)
    No longer failing if run on a project without a master branch

--- tests/test_git_prune.py
+++ tests/test_git_prune.py
@@ -1,44 +1,53 @@
 from git_prune import __version__
-from git_prune import main
-import unittest
+from git_prune.main import GitPrune
 
 
-class GitPruneTests(unittest.TestCase):
+def setup_module(module):
+    global gitprune
+    print("*****SETUP*****")
+    gitprune = GitPrune()
+    gitprune.shell_cmd("git branch setup")
+    gitprune.not_remote.append("setup")
 
-    def setUp(self):
-        self.gitprune = main.GitPrune()
 
-    def test_version(self):
-        assert __version__ == '0.0.9'
+def teardown_module(module):
+    print("*****TEARDOWN*****")
 
-    def test_shell_CMD(self):
-        shell_CMD = self.gitprune.shell_cmd('echo testing123')
-        assert shell_CMD == 'testing123'
-        assert type(shell_CMD) == str
 
-    def test_get_remote_branches(self):
-        remote_branches = self.gitprune.get_remote_branches()
-        assert 'master' in remote_branches
-        assert type(remote_branches) == list
-        assert remote_branches != []
+def test_version():
+    assert __version__ == "0.0.9"
 
-    def test_get_local_branches(self):
-        local_branches = self.gitprune.get_local_branches()
-        assert 'master' in local_branches
-        assert type(local_branches) == list
-        assert local_branches != []
 
-    def test_not_remote(self):
-        assert type(self.gitprune.not_remote) == list
-        assert self.gitprune.not_remote == []
+def test_shell_CMD():
+    shell_CMD = gitprune.shell_cmd("echo testing123")
+    assert shell_CMD == "testing123"
+    assert type(shell_CMD) == str
 
-    def test_delete_branches(self):
-        self.gitprune.shell_cmd('git branch -c master new-branch')
-        assert 'new-branch' in self.gitprune.get_local_branches()
-        self.gitprune.shell_cmd('git checkout new-branch')
-        self.gitprune.not_remote.append('new-branch')
-        assert 'new-branch' in self.gitprune.not_remote
-        self.gitprune.delete_branches()
-        assert 'new-branch' not in self.gitprune.get_local_branches()
 
-    # def test_end_to_end(self):
+def test_get_remote_branches():
+    remote_branches = gitprune.get_remote_branches()
+    assert "master" in remote_branches
+    assert type(remote_branches) == list
+    assert remote_branches != []
+
+
+def test_get_local_branches():
+    local_branches = gitprune.get_local_branches()
+    assert "master" in local_branches
+    assert type(local_branches) == list
+    assert local_branches != []
+
+
+def test_not_remote():
+    assert type(gitprune.not_remote) == list
+    assert gitprune.not_remote == ["setup"]
+
+
+def test_delete_branches():
+    print(gitprune.shell_cmd("git branch new-branch"))
+    assert "new-branch" in gitprune.get_local_branches()
+    gitprune.shell_cmd("git checkout new-branch")
+    gitprune.not_remote.append("new-branch")
+    assert "new-branch" in gitprune.not_remote
+    gitprune.delete_branches()
+    assert "new-branch" not in gitprune.get_local_branches()
