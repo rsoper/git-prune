3cb73ebcd15f79cde028429e2ee930b229e1e9cd
Richard <richardwebdesigner@me.com>
2022-03-08 16:47:34 -0800

    Update dependencies and tests

--- tests/test_git_prune.py
+++ tests/test_git_prune.py
@@ -1,13 +1,8 @@
 from git_prune import __version__
-from git_prune.main import GitPrune
 
 
 def setup_module(module):
-    global gitprune
     print("*****SETUP*****")
-    gitprune = GitPrune()
-    gitprune.shell_cmd("git branch setup")
-    gitprune.not_remote.append("setup")
 
 
 def teardown_module(module):
@@ -18,32 +13,32 @@ def test_version():
     assert __version__ == "0.0.10"
 
 
-def test_shell_CMD():
+def test_shell_CMD(gitprune):
     shell_CMD = gitprune.shell_cmd("echo testing123")
     assert shell_CMD == "testing123"
     assert type(shell_CMD) == str
 
 
-def test_get_remote_branches():
+def test_get_remote_branches(gitprune):
     remote_branches = gitprune.get_remote_branches()
     assert "master" in remote_branches
     assert type(remote_branches) == list
     assert remote_branches != []
 
 
-def test_get_local_branches():
+def test_get_local_branches(gitprune):
     local_branches = gitprune.get_local_branches()
-    assert "master" in local_branches
+    assert "setup" in local_branches
     assert type(local_branches) == list
     assert local_branches != []
 
 
-def test_not_remote():
+def test_not_remote(gitprune):
     assert type(gitprune.not_remote) == list
     assert gitprune.not_remote == ["setup"]
 
 
-def test_delete_branches():
+def test_delete_branches(gitprune):
     print(gitprune.shell_cmd("git branch new-branch"))
     assert "new-branch" in gitprune.get_local_branches()
     gitprune.shell_cmd("git checkout new-branch")
