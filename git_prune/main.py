c319a239ee18d112643af45fea81c81f72827c31
Richard <richardwebdesigner@me.com>
2023-08-17 03:43:36 +0000

    Ci change

--- git_prune/main.py
+++ git_prune/main.py
@@ -1,17 +1,18 @@
+import re
 import shlex
 import subprocess
-import re
-import click
 import sys
 from pathlib import Path
 
+import click
 
-class GitPrune(object):
+
+class GitPrune:
     def __init__(self, **kwargs):
         self.working_directory = kwargs["dir_"] if ("dir_" in kwargs) else Path.cwd()
         self.is_git = (
             True
-            if (self.working_directory / ".git").exists()
+            if (Path(self.working_directory) / ".git").exists()
             else sys.exit("Directory does not contain .git directory.")
         )
         self.git = (
@@ -21,9 +22,51 @@ class GitPrune(object):
         )
 
         self.shell_cmd(f"{self.git} fetch -p")
-        self.remote_branches = self.get_remote_branches()
-        self.local_branches = self.get_local_branches()
-        self.not_remote = list(set(self.local_branches) - set(self.remote_branches))
+
+    @property
+    def remote_branches(self) -> list[str]:
+        """All of the remote branches
+
+        Returns:
+            list[str]: List of all the remote branches
+        """
+        remote_branches = self.shell_cmd(f"{self.git} for-each-ref")
+        remote_branches = re.findall(r"refs/remotes/[a-zA-Z]*/(.*)", remote_branches)
+        for line in remote_branches:
+            if "HEAD" in line:
+                remote_branches.remove(line)
+            else:
+                pass
+        return remote_branches
+
+    @property
+    def local_branches(self) -> list[str]:
+        """All of the local branches
+
+        Returns:
+            list[str]: List of all the local branches
+        """
+        local_branches = self.shell_cmd(f"{self.git} for-each-ref")
+        local_branches = re.findall(r"refs/heads/(.*)", local_branches)
+        return local_branches
+
+    @property
+    def not_remote(self) -> list[str]:
+        """Branches that only exist on the local machine
+
+        Returns:
+            list[str]: List of the local only branches
+        """
+        return list(set(self.local_branches) - set(self.remote_branches))
+
+    @property
+    def current_branch(self) -> str:
+        """Get the current branch
+
+        Returns:
+            str: Short branch name
+        """
+        return self.shell_cmd("git symbolic-ref --short HEAD")
 
     def shell_cmd(self, cmd: str) -> str:
         """Execute a shell command
@@ -41,50 +84,48 @@ class GitPrune(object):
             )
             results = results.strip()
             return results.decode("UTF-8")
-        except subprocess.CalledProcessError as e:
-            errorText = e.output.strip()
-            return errorText.decode("UTF-8")
+        except subprocess.CalledProcessError as exc:
+            error_text = exc.output.strip()
+            return error_text.decode("UTF-8")
 
-    def get_remote_branches(self) -> list:
-        """Get branches currently on remote
+    def change_to_branch(self):
+        """Change to branch not marked for deletion"""
+        try:
+            if "master" in self.local_branches:
+                target_branch = "master"
+            if "main" in self.local_branches:
+                target_branch = "main"
+            if (
+                "main" not in self.local_branches
+                and "master" not in self.local_branches
+            ):
+                eligible_branches = self.remote_branches
+                target_branch = eligible_branches[0]
+        except IndexError as exc:
+            print(exc)
+            print(f"Current branch: {self.current_branch}")
+            print(f"Not remote: {self.not_remote}")
+            print(f"Local: {self.local_branches}")
+            print(f"Remote: {self.remote_branches}")
 
-        Returns:
-            list: List of remote branches
-        """
-        remote_branches = self.shell_cmd(f"{self.git} for-each-ref")
-        remote_branches = re.findall(r"refs/remotes/[a-zA-Z]*/(.*)", remote_branches)
-        for line in remote_branches:
-            if "HEAD" in line:
-                remote_branches.remove(line)
-            else:
-                pass
-        return remote_branches
-
-    def get_local_branches(self) -> list:
-        """Get branches currently on the local machine
-
-        Returns:
-            str: List of local branches
-        """
-        local_branches = self.shell_cmd(f"{self.git} for-each-ref")
-        local_branches = re.findall(r"refs/heads/(.*)", local_branches)
-        return local_branches
+        self.shell_cmd(f"git checkout {target_branch}")
 
     def delete_branches(self):
         """Delete the branches not on remote"""
-        self.shell_cmd(f"git checkout {self.get_remote_branches()[0]}")
+        if not self.not_remote:
+            return
+        if self.current_branch in self.not_remote:
+            self.change_to_branch()
         for branch in self.not_remote:
             self.shell_cmd(f'{self.git} branch -D "{branch}"')
 
     def prune_local_branches(self):
-        """
-        Confirm which branches will be removed. Remove them.
-        """
+        """Confirm which branches will be removed. Remove them."""
         if self.not_remote:
-            delete_branches = input(
-                f"Branch(es) {self.not_remote} do not exist in the origin repository. Would you like to delete them? y/n: "
+            user_prompt = input(
+                f"Branch(es) {self.not_remote} do not exist in the origin repository. Would you like to delete them? y/N: "
             )
-            if delete_branches == "y":
+            if user_prompt.lower() == "y":
                 self.delete_branches()
             else:
                 print(f"Branch(es) {self.not_remote} will not be deleted.")
@@ -109,4 +150,4 @@ def cli(dir_):
 
 
 if __name__ == "__main__":
-    cli()
+    cli("pwd")
--- tests/conftest.py
+++ tests/conftest.py
@@ -3,9 +3,10 @@ import pytest
 from git_prune.main import GitPrune
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="function")
 def gitprune():
-    gitprune = GitPrune()
-    gitprune.shell_cmd("git branch setup")
-    gitprune.not_remote.append("setup")
-    return gitprune
+    git_prune = GitPrune()
+    git_prune.shell_cmd("git branch setup")
+    yield git_prune
+    if git_prune.not_remote:
+        git_prune.delete_branches()
